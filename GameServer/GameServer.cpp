7fd44c736e00df05be1626c78ebfe45332b4ca15
신동엽 <90247013+tlsehdduq@users.noreply.github.com>
2023-02-09 03:01:17 +0900

    2023 02 08
     ~ CPU 파이프라인

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -6,56 +6,55 @@
 #include<vector>
 #include<mutex>
 #include<windows.h>
+#include<future>
 
-mutex m;
-queue<int32> q;
-HANDLE handle;
-void Producer()
+//가시성 , 코드 재배치 
+int32 x = 0;
+int32 y = 0;
+int32 r1 = 0;
+int32 r2= 0;
+
+volatile bool ready;
+
+void Thread_t1()
 {
-	while (true) {
+	while (!ready)
+		;
+	y = 1; //Store y
+	r1 = x; // Load x
 
-		{
-			unique_lock<mutex> lock(m);
-			q.push(100);
-
-		}
-
-		::SetEvent(handle); // signal상태로 바꿔주세요  true를 대입하는 느낌 
-
-		this_thread::sleep_for(10000ms);
-	}
 }
 
-void Consumer()
+void Thread_t2()
 {
-	while (true)
-	{
-		::WaitForSingleObject(handle, INFINITE);
-		//NON - signal
-		unique_lock<mutex> lock(m);
-		if (q.empty() == false)
-		{
-			int32 data = q.front();
-			q.pop();
-			cout << data << endl;
-		}
-	}
+	while (!ready)
+		;
+	x = 1; // Store x
+	r2 = y; // Load y
 }
+
 
 int main()
 {
-	//커널 오브젝트
-	// Usage count
-	// Signal ( 파란불 ) / Non_signal ( 빨간불 )  << bool 
-	// Auto // Manual << bool 
-	handle = ::CreateEvent(NULL/*보안속성*/, FALSE/*bManualReset*/, FALSE/*bInitialState*/, NULL);
-	
-	thread t1(Producer);
-	thread t2(Consumer);
+	int32 count = 0;
+	while (true)
+	{
+		ready = false;
+		count++;
 
-	t1.join();
-	t2.join();
+		x = y = r1 = r2 = 0;
+		thread t1(Thread_t1);
+		thread t2(Thread_t2);
 
-	::CloseHandle(handle);
+		ready = true;
+
+		t1.join();
+		t2.join();
+
+		if (r1 == 0 && r2 == 0)
+			break;
+	}
+	cout << count << " 번 만에 빠져나옴 " << endl;
+
 
 }
